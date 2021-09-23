79b5b21c148cc6f989e00abf9770b4edc803aeac
Silvio Silva <smsilva@gmail.com>
2021-09-23 01:37:23 -0300

    Getting started

---
+++ getting-started/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get install python3-pip --yes
+
+pip3 --version
+
+python3 -m pip install robotframework
+
+if ! grep --quiet "^export PATH=.*local/bin" ~/.bashrc; then
+  echo "export PATH=\${PATH}:\${HOME}/.local/bin" | tee -a ~/.bashrc
+fi
+
+robot --version
+
+pip3 install robotframework-requests
+
+robot tests.robot
+rebot output.xml
